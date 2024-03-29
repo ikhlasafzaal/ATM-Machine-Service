2c22ab861eb0edd7d49aa12f048d23c9e0a25020
ikhlasafzaal <ikhlasafzaal13@gmail.com>
2024-03-29 03:50:15 -0700

    FirstCommit

---
+++ README.md
@@ -0,0 +1,30 @@
+
+# ATM Service (TypeScript & Node.js)
+
+This command-line application, built with TypeScript and Node.js, simulates an ATM service, allowing users to perform basic banking operations such as checking balance, depositing funds, withdrawing funds, and fast cash withdrawal.
+
+## Features
+
+- **PIN Code Verification**: Users must enter their PIN code(9876) to access their account.
+- **Check Balance**: Users can check their account balance.
+- **Deposit**: Users can deposit funds into their account.
+- **Withdraw**: Users can withdraw funds from their account, provided they have sufficient balance.
+- **Fast Cash Out**: Users can quickly withdraw preset amounts of cash.
+
+## Prerequisites
+
+- Node.js installed on your machine.
+- npm (Node Package Manager) installed on your machine.
+
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## Accessing the Package on NPM
+You can access the package [here](https://www.npmjs.com/package/@ikhlasbano/atm-service-machine).
+
+
+
+
+Feel free to customize the README further to add any additional information or instructions you think would be helpful for users. Happy banking!
---
+++ index.js
@@ -0,0 +1,72 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let userBalance = 200000;
+let myPin = 9876;
+// PinCode Verification
+let pinCode = await inquirer.prompt({
+    name: "code",
+    type: "number",
+    message: "Enter your account PIN Code."
+});
+if (pinCode.code === myPin) {
+    console.log("Login Successfully. \nGreetings! You've entered your account.");
+    let atmServices = await inquirer.prompt({
+        name: "action",
+        message: "Select an option",
+        type: "list",
+        choices: ["CheckBalance", "Deposit", "Withdraw", "FastCashout"]
+    });
+    // Checking User Balance
+    if (atmServices.action === "CheckBalance") {
+        console.log(`Your Current balance is: ${userBalance}`);
+    }
+    // ATM Deposit Services
+    else if (atmServices.action === "Deposit") {
+        let depositAmount = await inquirer.prompt({
+            name: "amount",
+            type: "number",
+            message: "Please specify the amount you'd like to deposit?"
+        });
+        userBalance += depositAmount.amount;
+        console.log("Transaction completed successfully!");
+        console.log(`Your Updated Balance is: ${userBalance}`);
+    }
+    // ATM Withdraw Services
+    else if (atmServices.action === "Withdraw") {
+        let withDrawAmount = await inquirer.prompt({
+            name: "amount",
+            type: "number",
+            message: "Please specify the withdrawal amount you'd like to request?"
+        });
+        if (withDrawAmount.amount <= userBalance) {
+            userBalance -= withDrawAmount.amount;
+            console.log("Transaction completed successfully!");
+            console.log(`Your Remaining Amount is: ${userBalance}`);
+        }
+        else {
+            console.log("Apologies, the transaction cannot proceed at this time. Insufficient funds.");
+            console.log(`Your current balance stands at: ${userBalance}`);
+        }
+    }
+    // Fast Cash Out Services
+    else if (atmServices.action === "FastCashout") {
+        let cashAmount = await inquirer.prompt({
+            name: "cash",
+            type: "list",
+            message: "Choose your Amount!",
+            choices: ["10000", "40000", "60000", "70000", "80000"]
+        });
+        if (cashAmount.cash <= userBalance) {
+            userBalance -= cashAmount.cash;
+            console.log("Transaction completed successfully!");
+            console.log(`Your Remaining Amount is: ${userBalance}`);
+        }
+        else {
+            console.log("Apologies, the transaction cannot proceed at this time. Insufficient funds.");
+            console.log(`Your current balance stands at: ${userBalance}`);
+        }
+    }
+}
+else {
+    console.log("Invalid PIN entered! Please attempt again.");
+}
